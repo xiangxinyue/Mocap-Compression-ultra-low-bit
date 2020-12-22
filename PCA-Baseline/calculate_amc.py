b4f96ea0e76afce30b42875b77356191bb5e9e59
Yongqin Jian <32945661+YongqinJian@users.noreply.github.com>
2020-12-21 21:48:35 -0700

    Update calculate_amc.py

--- PCA-Baseline/calculate_amc.py
+++ PCA-Baseline/calculate_amc.py
@@ -1,5 +1,8 @@
 import numpy as np
 from sklearn.decomposition import PCA
+import bezier
+import matplotlib.pyplot as plt
+
 # Ref: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
 
 # the class of animation deal with the movements
@@ -86,79 +89,102 @@ class Frame():
 
 
 
+
+
+
 if __name__ == "__main__":
     walk_amc = Animation("modify.amc")
     joint_order = walk_amc.get_joint_order()
 
     # list all the 29 joints by the index
-    root = joint_order[0]
-    lowerback = joint_order[1]
-    upperback = joint_order[2]
-    thorax = joint_order[3]
-    lowerneck = joint_order[4]
-    upperneck = joint_order[5]
-    head = joint_order[6]
-    rclavicle = joint_order[7]
-    rhumerus = joint_order[8]
-    rradius = joint_order[9]
-    rwrist = joint_order[10]
-    rhand = joint_order[11]
-    rfingers = joint_order[12]
-    rthumb = joint_order[13]
-    lclavicle = joint_order[14]
-    lhumerus = joint_order[15]
-    lradius = joint_order[16]
-    lwrist = joint_order[17]
-    lhand = joint_order[18]
-    lfingers = joint_order[19]
-    lthumb = joint_order[20]
-    rfemur = joint_order[21]
-    rtibia = joint_order[22]
-    rfoot = joint_order[23]
-    rtoes = joint_order[24]
-    lfemur = joint_order[25]
-    ltibia = joint_order[26]
-    lfoot = joint_order[27]
-    ltoes = joint_order[28]
+    # root = joint_order[0]
+    # lowerback = joint_order[1]
+    # upperback = joint_order[2]
+    # thorax = joint_order[3]
+    # lowerneck = joint_order[4]
+    # upperneck = joint_order[5]
+    # head = joint_order[6]
+    # rclavicle = joint_order[7]
+    # rhumerus = joint_order[8]
+    # rradius = joint_order[9]
+    # rwrist = joint_order[10]
+    # rhand = joint_order[11]
+    # rfingers = joint_order[12]
+    # rthumb = joint_order[13]
+    # lclavicle = joint_order[14]
+    # lhumerus = joint_order[15]
+    # lradius = joint_order[16]
+    # lwrist = joint_order[17]
+    # lhand = joint_order[18]
+    # lfingers = joint_order[19]
+    # lthumb = joint_order[20]
+    # rfemur = joint_order[21]
+    # rtibia = joint_order[22]
+    # rfoot = joint_order[23]
+    # rtoes = joint_order[24]
+    # lfemur = joint_order[25]
+    # ltibia = joint_order[26]
+    # lfoot = joint_order[27]
+    # ltoes = joint_order[28]
 
     clip_size = 360
-    i = 0 
-    for i in range(29):
+    i = 0
+    checker = True
+    for i in range(29): # for each joint
         clip = walk_amc.get_trajectory_all_dof_cut_by_k(joint_order[i], clip_size)
-        print(clip.shape)
+        #print(clip.shape)
 
         # class sklearn.decomposition.PCA(n_components=None, *, copy=True, whiten=False, svd_solver='auto', tol=0.0, iterated_power='auto', random_state=None)[source]¶
         pca = PCA(n_components=0.95)
 
-        for i in range(clip.shape[0]):
-            c = clip[i]
+        for j in range(clip.shape[0]):  # no sense loop
+            c = clip[j]
             pca.fit(c)
-            print(c.shape)
+            #print(c.shape)
 
             #explained_variance_ratio_array, shape (n_components,)
             # Percentage of variance explained by each of the selected components.
             # If n_components is not set then all components are stored and the sum of the ratios is equal to 1.0.
-            print(pca.explained_variance_ratio_)
+
+            #print(pca.explained_variance_ratio_)
 
             # explained_variance_array, shape (n_components,) 
             # The amount of variance explained by each of the selected components.
             # Equal to n_components largest eigenvalues of the covariance matrix of X.
-            print(pca.explained_variance_)
+
+            #print(pca.explained_variance_)
 
             # n_componentsint, float, None or str
             # Number of components to keep
-            print(pca.n_components_)
+
+            #print(pca.n_components_)
 
             #components_array, shape (n_components, n_features)
             # Principal axes in feature space, representing the directions of maximum variance in the data. 
-            # The components are sorted by explained_variance_.
-            print(pca.components_)
+            # The components are sorted by explained_variance_.\
+
+            #print(pca.components_)
             new_x = pca.fit_transform(c)
+            # print(new_x.shape)
+            # print(len(new_x))
+
+            if i==9:
+                temp = []
+                for ind in range(360):
+                    temp.append(ind)
+                temp = np.array(temp)
+                nodes = np.asfortranarray((temp,new_x[:,0]))
+                curve = bezier.Curve(nodes,degree=359)
+                curve.plot(360)
+
+                plt.show()
+
             x = pca.inverse_transform(new_x)
-            print(new_x)
+            #print(new_x)
 
             # mean_array, shape (n_features,)
             # Per-feature empirical mean, estimated from the training set.
             # Equal to X.mean(axis=0).
-            error = np.mean((x - c)**2)
-            print(error)
+
+            #error = np.mean((x - c)**2)
+            #print(error)
